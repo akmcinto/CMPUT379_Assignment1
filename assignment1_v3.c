4325033b01091e9cda21fa53c019c161649dbb06
Andrea McIntosh <akmcinto@ualberta.ca>
2015-09-29 16:19:17 -0600

    Logging number of processes killed seems correct

--- assignment1_v3.c
+++ assignment1_v3.c
@@ -5,13 +5,17 @@
 #include <signal.h>
 #include <time.h>
 #include <string.h>
+#include <unistd.h>
+#include "memwatch.h"
 
 int main(int argc, char *argv[])
 {
+  mwInit();
+
   FILE *LOGFILE = fopen("./logfile.txt", "w");
   // Open file
   FILE *f = fopen(argv[1], "r");
-  int status;
+  int status = 0;
   // Array for holding each line of the file read in
   char procname[255]; // for saving read from file
   char cmdline[269]; // for creating pgrep command (255 plus extra for command)
@@ -20,6 +24,8 @@ int main(int argc, char *argv[])
   FILE *pp;
   pid_t procid;
   time_t currtime;
+  pid_t childpids[255];
+  int childcount = 0;
   
   fscanf(f, "%i", &numsecs);
 
@@ -29,17 +35,18 @@ int main(int argc, char *argv[])
     pp = popen(cmdline, "r");
 
     while (fscanf(pp, "%d", &procid) != EOF) {
+      
+      // Initialize monitoring in log file
+      time(&currtime);
+      fprintf(LOGFILE, "[%.*s] Info: Initializing monitoring process %s (PID %d)\n", (int) strlen(ctime(&currtime))-1, ctime(&currtime), procname, procid);
+	
+      fflush(LOGFILE);
 
       if ((pid = fork()) < 0) {
 	printf("fork() error!");
       } 
 
       else if (pid == 0) {  // Child process
-	// Initialize monitoring in log file
-	time(&currtime);
-	fprintf(LOGFILE, "[%.*s] Info: Initializing monitoring process %s (PID %d)\n", (int) strlen(ctime(&currtime))-1, ctime(&currtime), procname, procid);
-	
-	fflush(LOGFILE);
 	// Wait for amount of time
 	sleep(numsecs);
 
@@ -54,21 +61,28 @@ int main(int argc, char *argv[])
 	exit(7);       
 	
       } 
-
       else {
-	//printf("Parent process\n"); 
-      }
-
-      while (wait(&status) != pid) {
-	//printf("wait error");
-	;
-      }
-      kill(pid, SIGTERM);
-      
-    }
-
-    
+	childpids[childcount] = pid;
+	//printf("%d\n", childpids[childcount]);
+	childcount++;
+	continue; 
+      }      
+    }  
   }
+  
+  int i;
+  for (i = 0; i < childcount; i++) {
+    if (wait(&status) != childpids[i]) {
+      ;
+    }
+  }
+
+  fprintf(LOGFILE, "[%.*s] Info: Exiting. %d process(es) killed.\n", (int) strlen(ctime(&currtime))-1, ctime(&currtime), childcount);
+
+  //kill(pid, SIGTERM);
+  
+  fflush(LOGFILE);
   fclose(LOGFILE); 
+  mwTerm();
   return 0;
 }
